41ee0e1b4ce3337bb60262f625c726d4cf8154ba
pradip <pradip.kharal777@gmail.com>
2024-08-11 16:11:04 +0545

    validation

--- src/api/v1/validation/webhook.ts
+++ src/api/v1/validation/webhook.ts
@@ -1,8 +1,22 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export const webHookBody = yup.object({
-   body: yup.object({
+  body: yup.object({
     challenge: yup.string().optional(),
-    event: yup.object().optional(),
-   }),
-})
+    event: yup
+      .object()
+      .shape({
+        value: yup
+          .object()
+          .shape({
+            value: yup.number().required(),
+          })
+          .required(),
+        columnId: yup.string().required(),
+        boardId: yup.number().required(),
+        pulseId: yup.number().required(),
+      })
+      .optional()
+      .default(undefined)
+  }),
+});
