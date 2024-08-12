b9fa526a136fe9146b35fa38249220aa678e5616
pradip <pradip.kharal777@gmail.com>
2024-08-12 19:42:00 +0545

    test added

--- __test__/webhook.spec.ts
+++ __test__/webhook.spec.ts
@@ -1,12 +1,15 @@
-import supertest from 'supertest';
-import { Server } from '../src/api';
+import supertest from "supertest";
+import { Server } from "../src/api";
 
-const server = new Server().getApp()
+const server = new Server().getApp();
 
-describe('Check Webhook', () => {
-  describe('send event', () => {
-    it('should return status code 200', async () => {
-      await supertest(server).post('/api/v1/webhook/update_column_value').send({event:""}).expect(200);
+describe("Check Webhook", () => {
+  describe("send event", () => {
+    it("should return status code 200", async () => {
+      await supertest(server)
+        .post("/api/v1/webhook/update_column_value")
+        .send({ challenge: "token" })
+        .expect(200);
     });
   });
 });
